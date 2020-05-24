78f7c5eadbc82062bd3df1c55d41323d95cef718
Матвей Мурашко <matsvei.murashka@wolterskluwer.com>
2020-05-25 01:39:02 +0300

    updated base compoonents

--- test/page_objects/base_elements/base_collection.js
+++ test/page_objects/base_elements/base_collection.js
@@ -1,24 +1,25 @@
 const { logger } = require('../../config/logger_config');
 
 class Collection {
-  constructor(collectionName, selector) {
+  constructor(collectionName, selector, elementChildren = []) {
     this.collection = element.all(by.css(selector));
-    this.collectionName = collectionName;
+    this.name = collectionName;
+    this.children = elementChildren;
   };
 
   async getCount() {
     const collectionCount = await this.collection.count();
-    logger.debug(`Count of "${this.collectionName}" is "${collectionCount}"`);
+    logger.debug(`Count of "${this.name}" is "${collectionCount}"`);
     return collectionCount;
   };
 
   async getText() {
     const arrayOfCollectionTexts = await this.collection.getText();
-    logger.debug(`Texts of "${this.collectionName}" are [${arrayOfCollectionTexts}]`);
+    logger.debug(`Texts of "${this.name}" are [${arrayOfCollectionTexts}]`);
     if (arrayOfCollectionTexts.length !== 0) {
       return arrayOfCollectionTexts;
     } else {
-      throw new Error(`No texts found in [${this.collectionName}]`);
+      throw new Error(`No texts found in [${this.name}]`);
     }
   };
 
@@ -35,7 +36,7 @@ class Collection {
 
   async clickElementByText(text) {
     const indexOfElementToClick = await this.getElementIndexByText(text);
-    logger.debug(`Clicking [${this.collectionName}] element with [${indexOfElementToClick}] index`)
+    logger.debug(`Clicking [${this.name}] element with [${indexOfElementToClick}] index`)
     return this.collection.get(indexOfElementToClick).click();
   };
 };
--- test/page_objects/base_elements/base_element.js
+++ test/page_objects/base_elements/base_element.js
@@ -1,19 +1,20 @@
 const { logger } = require('../../config/logger_config');
 
 class Element {
-  constructor(elementName, selector) {
+  constructor(elementName, selector, arrayOfChildElements = []) {
     this.element = element(by.css(selector));
-    this.elementName = elementName;
+    this.name = elementName;
+    this.children = arrayOfChildElements;
   };
 
   click() {
-    logger.debug(`Clicking "${this.elementName}"`);
+    logger.debug(`Clicking "${this.name}"`);
     return this.element.click();
   };
 
   async getText() {
     const elementText = await this.element.getText();
-    logger.debug(`"${this.elementName}" element text is [${elementText}]`);
+    logger.debug(`"${this.name}" element text is [${elementText}]`);
     return elementText;
   };
 };
