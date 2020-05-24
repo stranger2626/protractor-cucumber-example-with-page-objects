c0cf89fbbd0ecc704ac9a5413c4e2648666d77ce
Матвей Мурашко <matsvei.murashka@wolterskluwer.com>
2020-05-25 02:48:49 +0300

    improved BasePage class

--- test/page_objects/base_page.js/base_page.js
+++ test/page_objects/base_page.js/base_page.js
@@ -1,8 +1,16 @@
 const { logger } = require('../../config/logger_config');
+const Element = require('../base_elements/base_element');
+const Collection = require('../base_elements/base_collection');
 
 class BasePage {
-  constructor() {
+  constructor(pageAlias, pageUrl) {
+    this.pageAlias = pageAlias;
+    this.pageUrl = pageUrl;
+    this.pageElements = [
+      new Element('Contact Us Button', '.cta-button-ui')
+    ];
   };
+
   wait(waitTimeInMilliseconds) {
     logger.debug(`Waiting [${waitTimeInMilliseconds}] milliseconds`);
     return browser.sleep();
@@ -17,6 +25,11 @@ class BasePage {
   openUrl(url) {
     logger.debug(`Opening [${url}] url`);
     return browser.get(url);
+  };
+
+  open() {
+    logger.debug(`Opening [${this.pageUrl}] url`);
+    return browser.get(this.pageUrl);
   };
 };
 
